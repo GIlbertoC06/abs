ec4988204d2670e988b20bc0d4877d8ef0b59b99
Gilberto <gcalvillo530@gmail.com>
2023-06-27 00:07:40 -0700

    https://github.com/GIlbertoC06/LightBulb.git

---
+++ Lightbulb/app.js
@@ -0,0 +1,9 @@
+function bulb_on(){
+    document.getElementById('bulb').src='https://cdn.britannica.com/41/129641-050-D30CA107/lightbulb-invention-Thomas-Alva-Edison-1879.jpg';
+}
+function bulb_off(){
+    document.getElementById('bulb').src='https://media.istockphoto.com/id/502789173/photo/light-bulb-concept.jpg?s=612x612&w=0&k=20&c=vJfSDuFJf_4UsQcUsdMNUfDkkNU6Bf9cTHF-n4-Zwvs=';
+}
+
+var msg = "Hello World" ;
+console.log(msg);
